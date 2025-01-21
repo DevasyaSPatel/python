0b1a2358127ce4384f52d2f6041f48c98cfc13b2
DevasyaSPatel <156588183+DevasyaSPatel@users.noreply.github.com>
2025-01-21 14:23:52 +0530

    Delete 1.23.py

--- 1.23.py
+++
@@ -1,7 +0,0 @@
-s1=int(input("enter the marks in this subjects "))
-s2=int(input("enter the marks in this subjects "))
-s3=int(input("enter the marks in this subjects "))
-total=s1+s2+s3
-avg=total/3
-print("total of all subjects is ", total)
-print("average of the subjects is ",avg)
