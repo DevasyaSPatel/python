4ee882346e4fb51af609d55e37a331e5fb9138a0
DevasyaSPatel <156588183+DevasyaSPatel@users.noreply.github.com>
2025-02-03 22:13:33 +0530

    Update 3.3.py

--- lab3/3.3.py
+++ lab3/3.3.py
@@ -1,13 +1,10 @@
 x=input("enter the string 1 ")
 y=input("enter the string 2 ")
-def check(x,y):
-    for i in x:
-        if i==y[0]:
-            for j in y :
-                if i == j:
-                    print("yes")
-                else:
-                    break
-check(x,y)
 
+if x in y:
+    print("this is the string presnt inside ",x)
+elif y in x:
+    print("this is the string presnt inside ",y)
+else:
+    print("no string present inside any of them")
 
