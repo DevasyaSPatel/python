a79bc47b6d41592c1fdc513d19acb882f609f2c3
DevasyaSPatel <156588183+DevasyaSPatel@users.noreply.github.com>
2025-04-23 11:20:32 +0530

    Update 1.24.py

--- lab1/1.24.py
+++ lab1/1.24.py
@@ -1,5 +1,5 @@
-s1=int(input("enter the marks in this subjects "))
-s2=int(input("enter the marks in this subjects "))
-print(s1, "\n", s2)
+s1=int(input("enter value1 "))
+s2=int(input("enter value2 "))
+print("Before swap \n",s1, "\n", s2)
 s1,s2=s2,s1
-print(s1 ,"\n", s2)
+print("After swap \n",s1 ,"\n", s2)
