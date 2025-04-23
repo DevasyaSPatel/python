f977539a951ac94a6d463007e1cb38f3b200186a
DevasyaSPatel <156588183+DevasyaSPatel@users.noreply.github.com>
2025-04-23 13:02:46 +0530

    Add files via upload

---
+++ lab10/10.1.py
@@ -0,0 +1,5 @@
+import csv
+myfile=open("text.csv",'w')
+sw=csv.writer(myfile)
+myfile.close()
+
---
+++ lab10/10.8.py
@@ -0,0 +1,16 @@
+
+a = 'Story.txt'         
+b = 'NewStory.txt'
+
+remove = ['me', 'its', 'hey']
+
+with open(a, 'r', encoding='utf-8') as file:
+    content = file.read()
+
+for word in remove:
+    content = content.replace(f" {word} ", " ") 
+
+with open(b, 'w', encoding='utf-8') as file:
+    file.write(content)
+
+print(f"Words {', '.join(remove)} removed and saved to '{b}'.")
